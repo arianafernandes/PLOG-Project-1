a040ffea0a002e71a95befb3c7dcd78991169d66
Ariana <up201404789@fe.up.pt>
2016-11-13 04:30:36 +0000

    choosePieceToMove v1 with errors
    checkIsValidMove not done
    MoviePiece not done

--- CrabStack/src/CrabPvP.pl
+++ CrabStack/src/CrabPvP.pl
@@ -4,3 +4,18 @@
 crabPvP:-
         %retractall(gameState),
         gameInit.
+
+choosePieceToMove(Player, Board, NewBoard):-
+    write('\n Please introduce the row and collumn of the piece you want to move'),
+    write('\n    Piece Row '), read(Row), Row > 0, Row =< 5,
+    write('\n    Piece Collumn  '), read(Col), Col > 0, Col =< 5,
+    write('\n Please introduce the row and collumn where you want to move your piece'),
+    write('\n    Piece Row '), read(Row), Row > 0, Row =< 5,
+    write('\n    Piece Collumn  '), read(Col), Col > 0, Col =< 5,
+    (checkIsValidMove(Piece,Row, Col,RowDest,ColDest, Player, Board) ->
+                    MovePiece(Piece,Row,Col,RowDest,ColDest,Player,Board,NewBoard);
+                    displayMessage(['\nYou have to choose an available Play!']),
+                    choosePieceToMove(Player, Board, NewBoard))                                   
+                ;
+        displayMessage(['\n Cant move that piece! Please choose one of your pieces! ']),
+        choosePieceToMove(Player, Board, NewBoard)).
